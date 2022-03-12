c788253d44b0c7d19756c9b23a1bf07611841de1
Mohsin <44529081+MohsinEjaz21@users.noreply.github.com>
2022-03-12 12:59:37 +0500

    first commit

--- pages/CssUtility/index.tsx
+++ pages/CssUtility/index.tsx
@@ -5,52 +5,6 @@ var extractor = require('css-color-extractor');
 var Color = require('easy-color');
 
 
-function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-}
-
-function replaceAll(str, find, replace) {
-  return str.replaceAll(find, replace);
-}
-
-function OverrideAlert() {
-  return (
-    <div className="alert alert-warning" role="alert">
-      <strong>Warning!</strong> You are overriding a color.
-    </div>
-  );
-}
-
-function countMatch(str, find) {
-  return str.match(new RegExp(escapeRegExp(find), 'g'));
-}
-const uniqueArray = (arr) => arr.filter(function (item, pos) {
-  return arr.indexOf(item) == pos;
-})
-
-function sortColorBasedOnCount(a, b) {
-  var countA = a.count;
-  let countB = b.count;
-  if (countA < countB) return 1;
-  if (countA > countB) return -1;
-  return 0;
-}
-
-function copyToClipBoard() {
-  var copyText = document.getElementById("finalContent");
-  navigator.clipboard.writeText(copyText!.innerText)
-
-  // swal for 2s  then close
-  swal({
-    title: "Copied!",
-    text: "Your CSS is copied to clipboard",
-    icon: "success",
-    timer: 1000,
-    buttons: [false]
-  });
-
-}
-
 export default function CssTransform() {
 
   const [inputCss, setInputCss] = useState(FakeTemplates[0]);
@@ -59,69 +13,13 @@ export default function CssTransform() {
   const [fakeTemplateIndex, setFakeTemplateIndex] = useState(0);
 
 
-  function changeTemplate() {
-    let fakeIndex = fakeTemplateIndex;
-    if (fakeIndex == FakeTemplates.length - 1) {
-      fakeIndex = 0;
-    } else {
-      fakeIndex++;
-    }
-
-    // console.log(fakeIndex);
-
-    setFakeTemplateIndex(fakeIndex);
-    setInputCss(FakeTemplates[fakeIndex])
-  }
-
   function handleTransform() {
     let tempCss = inputCss;
     let colorIndex = 1;
     let namedColors = Object.keys(CssNamedColors);
     // Correcting css colors so that only get true colors when extract
-    tempCss = tempCss.split('\n').map(line => {
 
-      if (line.indexOf('url(https://1.www.s81c.com/common/v17e/i/buttons/btn-sprite.png) no-repeat 0 0 transparent') > -1) {
-        // console.log("here ")
-
-        //     console.log(`
-        //   line: ${line}
-        //   ${line.indexOf(':')} 
-        //   ${line.indexOf('{')}
-        //   ${line.indexOf(':hover')}
-        //   ${line.indexOf(':active')}
-        //   ${line.indexOf(':visited')}
-        //   ${line.indexOf(':focus')}
-        //   ${line.substring(line.indexOf(':') + 1)}
-        // `)
-      }
-
-      if (line.indexOf(':') > -1
-        && line.indexOf('{') === -1
-        && line.indexOf(':hover') === -1
-        && line.indexOf(':active') === -1
-        && line.indexOf(':visited') === -1
-        && line.indexOf(':focus') === -1
-      ) {
-        let secondPart = line.substring(line.indexOf(':') + 1);
-
-        if (secondPart.indexOf('rgb') > -1 && secondPart.indexOf(',') > -1) {
-          line = replaceAll(line, ', ', ',');
-          line = replaceAll(line, ' ,', ',');
-        }
-
-        namedColors.forEach(color => {
-          let regex = new RegExp('\\b' + color + '\\b');
-          let regex1 = new RegExp('\\b' + color + '-' + '\\b');
-          let regex2 = new RegExp('\\b' + '-' + color + '\\b');
-
-          if (secondPart.match(regex) && !secondPart.match(regex1) && !secondPart.match(regex2)) {
-            line = replaceAll(line, color, CssNamedColors[color]);
-          }
-        })
-
-      }
-      return line;
-    }).join('\n');;
+    tempCss = replaceNamedColorWithHex(tempCss, namedColors);
 
     // setOutputCss(tempCss);
     // return
@@ -132,12 +30,7 @@ export default function CssTransform() {
     if (generatedContentArea) {
       generatedContentArea.style.opacity = '0';
     }
-    let tempColors = extractor.fromCss(tempCss).filter((colorVal: any) => colorVal !== '0');
-    tempColors = uniqueArray(tempColors);
-    let hexColors = tempColors.filter((colorVal: any) => colorVal.indexOf('#') !== -1);
-    let otherThanHexColors = tempColors.filter((colorVal: any) => colorVal.indexOf('#') == -1);
-    hexColors.sort((a, b) => b.length - a.length)
-    tempColors = [...hexColors, ...otherThanHexColors];
+    let tempColors = removeDuplicateAndSortColors(tempCss);
 
     // tempColors.forEach((colorVal: any, index) => {
     //   console.log(`new_color_${index}`, colorVal);
@@ -147,7 +40,7 @@ export default function CssTransform() {
       let color = colorVal.replace(/\s/g, '');
       let currCssVar = `--color__${colorIndex}`;
       let colorName = `var(${currCssVar})`;
-      let matchColorLen = (countMatch(tempCss, color) || []).length
+      let matchColorLen = (countMatchColors(tempCss, color) || []).length
       let tempColorObj = {}
       let parser = new Color(colorVal);
       let rgbColor = parser.toRGBA();
@@ -181,7 +74,7 @@ export default function CssTransform() {
       }
     }, 200);
 
-    tempColorsArr.sort(sortColorBasedOnCount);
+    tempColorsArr.sort(sortColorByCount);
     setOutputCss(tempCss);
     setColorArr(tempColorsArr);
   }
@@ -200,7 +93,7 @@ export default function CssTransform() {
 
 
           <div className="action-btns" >
-            <button className="btn btn-primary" onClick={changeTemplate}>
+            <button className="btn btn-primary" onClick={fakeTemplateChange}>
               Use Fake CSS
             </button>
             <button className="margin-left-15px btn btn-primary" onClick={() => setInputCss('/* Paste Your Css */')} >
@@ -275,6 +168,100 @@ export default function CssTransform() {
     </div>
   )
 
+
+
+
+
+  function removeDuplicateAndSortColors(tempCss: string) {
+    let tempColors = extractor.fromCss(tempCss).filter((colorVal: any) => colorVal !== '0');
+    tempColors = makeArrayUnique(tempColors);
+    let hexColors = tempColors.filter((colorVal: any) => colorVal.indexOf('#') !== -1);
+    let otherThanHexColors = tempColors.filter((colorVal: any) => colorVal.indexOf('#') == -1);
+    hexColors.sort((a, b) => b.length - a.length);
+    tempColors = [...hexColors, ...otherThanHexColors];
+    return tempColors;
+  }
+
+  function replaceNamedColorWithHex(tempCss: string, namedColors: string[]) {
+    tempCss = tempCss.split('\n').map(line => {
+      // tempLogs(line)
+      if (line.indexOf(':') > -1
+        && line.indexOf('{') === -1
+        && line.indexOf(':hover') === -1
+        && line.indexOf(':active') === -1
+        && line.indexOf(':visited') === -1
+        && line.indexOf(':focus') === -1) {
+        let secondPart = line.substring(line.indexOf(':') + 1);
+
+        if (secondPart.indexOf('rgb') > -1 && secondPart.indexOf(',') > -1) {
+          line = replaceAll(line, ', ', ',');
+          line = replaceAll(line, ' ,', ',');
+        }
+
+        namedColors.forEach(color => {
+          let regex = new RegExp('\\b' + color + '\\b');
+
+          let regex1 = new RegExp('\\b' + color + '-' + '\\b');
+          let regex2 = new RegExp('\\b' + '-' + color + '\\b');
+
+          if (secondPart.match(regex) && !secondPart.match(regex1) && !secondPart.match(regex2)) {
+            line = replaceAll(line, color, CssNamedColors[color]);
+          }
+        });
+
+      }
+      return line;
+    }).join('\n');
+    return tempCss;
+  }
+
+  function fakeTemplateChange() {
+    let fakeIndex = fakeTemplateIndex;
+    if (fakeIndex == FakeTemplates.length - 1) {
+      fakeIndex = 0;
+    } else {
+      fakeIndex++;
+    }
+    setFakeTemplateIndex(fakeIndex);
+    setInputCss(FakeTemplates[fakeIndex])
+  }
+
+  function copyToClipBoard() {
+    var copyText = document.getElementById("finalContent");
+    navigator.clipboard.writeText(copyText!.innerText)
+    swal({
+      title: "Copied!",
+      text: "Your CSS is copied to clipboard",
+      icon: "success",
+      timer: 1000,
+      buttons: [false]
+    });
+  }
+
+  function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
+
+  function replaceAll(str, find, replace) {
+    return str.replaceAll(find, replace);
+  }
+
+  function countMatchColors(str, find) {
+    return str.match(new RegExp(escapeRegExp(find), 'g'));
+  }
+
+  function makeArrayUnique(arr) {
+    return arr.filter((item, pos) => arr.indexOf(item) == pos)
+  };
+
+  function sortColorByCount(a, b) {
+    var countA = a.count;
+    let countB = b.count;
+    if (countA < countB) return 1;
+    if (countA > countB) return -1;
+    return 0;
+  }
+
   function outputCssMessage() {
     return <><div className="alert-message">
       {`/* ColorLength is ${colorArr.length} \n Note Point :: Named colors are transform into \n respective hex format to avoid color repetition \n Original Colors are below */ `}
@@ -286,17 +273,19 @@ export default function CssTransform() {
       {`/* ======================== \n Override colors with Rgba \n ======================== */ `}
     </div><br /></>
   }
+
+  function tempLogs(line) {
+    if (line.indexOf('url(https://1.www.s81c.com/common/v17e/i/buttons/btn-sprite.png) no-repeat 0 0 transparent') > -1) {
+      console.log(`
+    line: ${line}
+    ${line.indexOf(':')} 
+    ${line.indexOf('{')}
+    ${line.indexOf(':hover')}
+    ${line.indexOf(':active')}
+    ${line.indexOf(':visited')}
+    ${line.indexOf(':focus')}
+    ${line.substring(line.indexOf(':') + 1)}
+    `)
+    }
+  }
 }
-
-
-
-        // line = exactMatchReplace(line, 'black', CssNamedColors.black);
-
-        // if (line.split(':')[0].indexOf('white') < 0) {
-        //   line = exactMatchReplace(line, 'white', CssNamedColors.white);
-        // }
-        // if (line.includes('transparent')) {
-        //   line = exactMatchReplace(line, 'transparent', "rgba(0,0,0,0)");
-        // }
-            // line = line.replace(new RegExp(escapeRegExp(color), 'g'), CssNamedColors[color]);
-
