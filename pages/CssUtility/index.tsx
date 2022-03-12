84781e138e8a9a83ce178958a58924b22756eea7
Mohsin <44529081+MohsinEjaz21@users.noreply.github.com>
2022-03-12 15:21:31 +0500

    first commit

--- pages/CssUtility/index.tsx
+++ pages/CssUtility/index.tsx
@@ -4,8 +4,7 @@ import { CssNamedColors, FakeTemplates } from '../../inputcss';
 var extractor = require('css-color-extractor');
 var Color = require('easy-color');
 
-
-export default function CssTransform() {
+export default function CssTransformIndex() {
 
   const [inputCss, setInputCss] = useState(FakeTemplates[0]);
   const [outputCss, setOutputCss] = useState('');
@@ -16,42 +15,52 @@ export default function CssTransform() {
   function handleTransform() {
     let tempCss = inputCss;
     let colorIndex = 1;
-    let namedColors = Object.keys(CssNamedColors);
-    // Correcting css colors so that only get true colors when extract
-
-    tempCss = replaceNamedColorWithHex(tempCss, namedColors);
-
-    // setOutputCss(tempCss);
-    // return
-    // console.log("tempCss", tempCss);
-
     let tempColorsArr: any = [];
-    let generatedContentArea: any = document.querySelector('div.right-section > pre');
-    if (generatedContentArea) {
-      generatedContentArea.style.opacity = '0';
-    }
+
+    tempCss = replaceNamedColorWithHex(tempCss);
+    let generatedContentArea: any = hideOutputCss();
     let tempColors = removeDuplicateAndSortColors(tempCss);
 
-    // tempColors.forEach((colorVal: any, index) => {
-    //   console.log(`new_color_${index}`, colorVal);
-    // })
+    // printColors(tempColors);
 
-    tempColors = tempColors.map((colorVal: any, index: number) => {
+    ({ colorIndex, tempCss } = replaceColorWithVariableNameInCss(tempColors, colorIndex, tempCss, tempColorsArr));
+
+    showOutputCss(generatedContentArea);
+    tempColorsArr.sort(sortColorByCountDesc);
+    setOutputCss(tempCss);
+    setColorArr(tempColorsArr);
+  }
+
+  return (
+    <div className="container">
+      <InputCssJsx inputCss={inputCss} setInputCss={setInputCss} fakeTemplateChange={fakeTemplateChange} ></InputCssJsx>
+      <OutputCssJsx outputCss={outputCss} colorArr={colorArr} handleTransform={handleTransform} ></OutputCssJsx>
+    </div>
+  )
+
+
+  function replaceColorWithVariableNameInCss(tempColors: any, colorIndex: number, tempCss: string, tempColorsArr: any) {
+    tempColors.forEach((colorVal: any, index: number) => {
       let color = colorVal.replace(/\s/g, '');
       let currCssVar = `--color__${colorIndex}`;
       let colorName = `var(${currCssVar})`;
-      let matchColorLen = (countMatchColors(tempCss, color) || []).length
-      let tempColorObj = {}
+      let matchColorLen = (countMatchColors(tempCss, color) || []).length;
+      let tempColorObj = {};
       let parser = new Color(colorVal);
       let rgbColor = parser.toRGBA();
 
-      let foundColorIndex = tempColorsArr.findIndex(color => color.value == rgbColor.toString())
-      if (foundColorIndex > -1) {
+      let foundColorIndex = tempColorsArr.findIndex(color => color.value == rgbColor.toString());
+      foundColorIndex > -1 ? colorAlreadyDeclared() : declareNewColor()
+      tempCss = replaceAll(tempCss, color, colorName);
+      tempCss = replaceAll(tempCss, '@charset "utf-8";', '');
+
+      function colorAlreadyDeclared() {
         currCssVar = tempColorsArr[foundColorIndex]['key'];
         colorName = `var(${currCssVar})`;
         tempColorsArr[foundColorIndex]['count'] = tempColorsArr[foundColorIndex]['count'] + matchColorLen;
       }
-      else {
+
+      function declareNewColor() {
         tempColorObj['key'] = currCssVar;
         tempColorObj['value'] = rgbColor;
         tempColorObj['original'] = colorVal;
@@ -59,118 +68,9 @@ export default function CssTransform() {
         tempColorsArr.push(tempColorObj);
         colorIndex++;
       }
-
-      tempCss = replaceAll(tempCss, color, colorName);
-      // tempCss = replaceAll(tempCss, `.${colorName}`, `.${colorVal}`);
-
-      return rgbColor;
     });
-
-    tempCss = replaceAll(tempCss, '@charset "utf-8";', '');
-
-    setTimeout(() => {
-      if (generatedContentArea) {
-        generatedContentArea.style.opacity = '1';
-      }
-    }, 200);
-
-    tempColorsArr.sort(sortColorByCount);
-    setOutputCss(tempCss);
-    setColorArr(tempColorsArr);
+    return { colorIndex, tempCss };
   }
-
-  return (
-    <div className="container">
-
-      {/* Text box for input css */}
-      <div className="svg hide" id="svg"></div>
-
-      <div className="left-section">
-        <div className="section-header">
-          <div className="heading-primary">
-            Enter Your CSS 🌸
-          </div>
-
-
-          <div className="action-btns" >
-            <button className="btn btn-primary" onClick={fakeTemplateChange}>
-              Use Fake CSS
-            </button>
-            <button className="margin-left-15px btn btn-primary" onClick={() => setInputCss('/* Paste Your Css */')} >
-              Paste Your Own
-            </button>
-          </div>
-
-
-        </div>
-        <textarea
-          className="inputcss-textarea content-area"
-          value={inputCss}
-          onChange={(e) => setInputCss(e.target.value)}
-        />
-      </div>
-      <div className="right-section" >
-        <div className="section-header">
-          <div className="heading-primary">
-            Dynamic CSS 🎉
-          </div>
-          <div className="action-btns" >
-            <button className="btn btn-primary" onClick={handleTransform}>
-              🌈 Make CSS Beutiful
-            </button>
-            <button className="margin-left-15px btn btn-primary" onClick={copyToClipBoard}>
-              ✅ Copy New CSS
-            </button>
-          </div>
-        </div>
-
-        <pre id="finalContent" className="generated-css content-area slideInRight">
-          <div>{colorArr.length > 0 && <>{outputCssMessage()}</>}</div>
-          <div>{colorArr.length > 0 && <>{`:root {`}</>}</div>
-
-          {colorArr.map((color: any, index) => {
-            return (
-              <>
-                <div key={index} className="color-variables floatLeft">
-                  {color.key}: {color.original};
-                </div>
-                <div>
-                  {` /*  ${color.count} */`}
-                </div>
-              </>
-            )
-          })}
-
-          <br />
-          {colorArr.length > 0 && <>{overrideColorMessage()}</>}
-          {colorArr.map((color: any, index) => {
-            return (
-              <>
-                <div key={index} className="color-variables floatLeft">
-                  {color.key}: {color.value};
-                </div>
-                <div >
-                  {` /* ${color.count} */`}
-                </div>
-              </>
-            )
-          })}
-          {colorArr.length > 0 && <div>{`}`}</div>}
-          <br />
-          {JSON.parse(JSON.stringify(outputCss, null, 2))}
-        </pre>
-
-
-      </div>
-
-
-
-    </div>
-  )
-
-
-
-
 
   function removeDuplicateAndSortColors(tempCss: string) {
     let tempColors = extractor.fromCss(tempCss).filter((colorVal: any) => colorVal !== '0');
@@ -182,7 +82,9 @@ export default function CssTransform() {
     return tempColors;
   }
 
-  function replaceNamedColorWithHex(tempCss: string, namedColors: string[]) {
+  function replaceNamedColorWithHex(tempCss: string) {
+    let namedColors = Object.keys(CssNamedColors);
+
     tempCss = tempCss.split('\n').map(line => {
       // tempLogs(line)
       if (line.indexOf(':') > -1
@@ -200,7 +102,6 @@ export default function CssTransform() {
 
         namedColors.forEach(color => {
           let regex = new RegExp('\\b' + color + '\\b');
-
           let regex1 = new RegExp('\\b' + color + '-' + '\\b');
           let regex2 = new RegExp('\\b' + '-' + color + '\\b');
 
@@ -226,18 +127,6 @@ export default function CssTransform() {
     setInputCss(FakeTemplates[fakeIndex])
   }
 
-  function copyToClipBoard() {
-    var copyText = document.getElementById("finalContent");
-    navigator.clipboard.writeText(copyText!.innerText)
-    swal({
-      title: "Copied!",
-      text: "Your CSS is copied to clipboard",
-      icon: "success",
-      timer: 1000,
-      buttons: [false]
-    });
-  }
-
   function escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   }
@@ -254,12 +143,98 @@ export default function CssTransform() {
     return arr.filter((item, pos) => arr.indexOf(item) == pos)
   };
 
-  function sortColorByCount(a, b) {
+  function sortColorByCountDesc(a, b) {
     var countA = a.count;
     let countB = b.count;
     if (countA < countB) return 1;
     if (countA > countB) return -1;
     return 0;
+  }
+
+  function showOutputCss(generatedContentArea: any) {
+    setTimeout(() => {
+      if (generatedContentArea) {
+        generatedContentArea.style.opacity = '1';
+      }
+    }, 200);
+  }
+
+  function hideOutputCss() {
+    let generatedContentArea: any = document.querySelector('div.right-section > pre');
+    if (generatedContentArea) {
+      generatedContentArea.style.opacity = '0';
+    }
+    return generatedContentArea;
+  }
+}
+
+
+
+function InputCssJsx(props) {
+  return (
+    <div className="left-section">
+      <div className="section-header">
+        <div className="heading-primary">
+          Enter Your CSS 🌸
+        </div>
+        <div className="action-btns">
+          <button className="btn btn-primary" onClick={props.fakeTemplateChange}>
+            Use Fake CSS
+          </button>
+          <button className="margin-left-15px btn btn-primary" onClick={() => props.setInputCss('/* Paste Your Css */')}>
+            Paste Your Own
+          </button>
+        </div>
+      </div>
+      <textarea className="inputcss-textarea content-area" value={props.inputCss} onChange={e => props.setInputCss(e.target.value)} />
+    </div>
+  );
+}
+
+function OutputCssJsx(prop) {
+  const { colorArr, handleTransform, outputCss } = prop;
+
+  return (<div className="right-section">
+    <div className="section-header">
+      <div className="heading-primary">
+        Dynamic CSS 🎉
+      </div>
+      <div className="action-btns">
+        <button className="btn btn-primary" onClick={handleTransform}>
+          🌈 Make CSS Beutiful
+        </button>
+        <button className="margin-left-15px btn btn-primary" onClick={copyToClipBoardOutputCss}>
+          ✅ Copy New CSS
+        </button>
+      </div>
+    </div>
+
+    <pre id="finalContent" className="generated-css content-area slideInRight">
+      {outputCss.length > 0 && (
+        <>
+          {outputCssMessage()}
+          {<div>{`:root {`} </div>}
+          {renderOriginalColors()}<br />
+          {overrideColorMessage()}<br />
+          {renderOverideColorsInRgba()}
+          {<div>{`}`} </div>}
+        </>
+      )}
+      {JSON.parse(JSON.stringify(outputCss, null, 2))}
+    </pre>
+  </div>);
+
+
+  function copyToClipBoardOutputCss() {
+    var copyText = document.getElementById("finalContent");
+    navigator.clipboard.writeText(copyText!.innerText)
+    swal({
+      title: "Copied!",
+      text: "Your CSS is copied to clipboard",
+      icon: "success",
+      timer: 1000,
+      buttons: [false]
+    });
   }
 
   function outputCssMessage() {
@@ -271,12 +246,45 @@ export default function CssTransform() {
   function overrideColorMessage() {
     return <><div className="alert-message">
       {`/* ======================== \n Override colors with Rgba \n ======================== */ `}
-    </div><br /></>
+    </div><br />
+    </>
   }
 
-  function tempLogs(line) {
-    if (line.indexOf('url(https://1.www.s81c.com/common/v17e/i/buttons/btn-sprite.png) no-repeat 0 0 transparent') > -1) {
-      console.log(`
+  function renderOriginalColors() {
+    return colorArr.map((color: any, index) => {
+      return <>
+        <div key={index} className="color-variables floatLeft">
+          {color.key}: {color.original};
+        </div>
+        <div>
+          {` /*  ${color.count} */`}
+        </div>
+      </>;
+    });
+  }
+
+  function renderOverideColorsInRgba() {
+    return colorArr.map((color: any, index) => {
+      return <>
+        <div key={index} className="color-variables floatLeft">
+          {color.key}: {color.value};
+        </div>
+        <div>
+          {` /* ${color.count} */`}
+        </div>
+      </>;
+    });
+  }
+
+}
+
+// =============
+// Xtra Logs
+// =============
+
+function tempLogs(line) {
+  if (line.indexOf('url(https://1.www.s81c.com/common/v17e/i/buttons/btn-sprite.png) no-repeat 0 0 transparent') > -1) {
+    console.log(`
     line: ${line}
     ${line.indexOf(':')} 
     ${line.indexOf('{')}
@@ -286,6 +294,11 @@ export default function CssTransform() {
     ${line.indexOf(':focus')}
     ${line.substring(line.indexOf(':') + 1)}
     `)
-    }
   }
+}
+
+function printColors(tempColors) {
+  tempColors.forEach((colorVal: any, index) => {
+    console.log(`new_color_${index}`, colorVal);
+  })
 }
